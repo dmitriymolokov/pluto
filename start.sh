8c79008c054f23aa1ed7a7afc13faa665a616227
Dmitriy Molokov <dmitriymolokov@users.noreply.github.com>
2022-08-23 21:11:32 +0300

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
 # terminal memcached
 sh terminate.sh
 
-sleep 3
+sleep 10
 
 # start new memcached
 echo 'starting new memcached instances'
-memcached -n 70 -m 4096 -M -t 12 -d
+memcached -n 70 -m 4096 -M -t 12 -d || {
+    echo 'memcached -n 70 -m 4096 -M -t 12 -d '; 
+    exit 1;
+}
 
-sleep 3
+sleep 10
 
 # load addresses in memcached
 echo 'loading addresses in memcached'
@@ -16,7 +19,7 @@ python3 fill.py || {
     exit 1;
 }
 
-sleep 3
+sleep 10
 
 # start magic
 echo 'loading magic'
