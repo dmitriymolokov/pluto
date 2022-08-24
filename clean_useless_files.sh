920a57750c51278026565085d46011676eaaf2e5
Dmitriy Molokov <dmitriymolokov@users.noreply.github.com>
2022-08-25 00:54:08 +0300

    wip

--- clean_useless_files.sh
+++
@@ -1,4 +0,0 @@
-echo 'removing old files if exists...'
-rm blockchair_bitcoin_addresses_and_balance_LATEST.tsv.gz
-rm blockchair_bitcoin_addresses_and_balance_LATEST.tsv
-echo 'removed'
--- update_data.sh
+++ update_data.sh
@@ -1,17 +1,47 @@
 # clean up
-sh clean_useless_files.sh
+clean_useless_files() {
+    echo 'removing old files if exist...'
+    rm blockchair_bitcoin_addresses_and_balance_LATEST.tsv.gz
+    rm blockchair_bitcoin_addresses_and_balance_LATEST.tsv
+    echo 'removed'
+}
+
+clean_useless_files
 
 # downloading new addresses
-sh download_data.sh
+echo 'downloading is started...'
+curl -O http://addresses.loyce.club/blockchair_bitcoin_addresses_and_balance_LATEST.tsv.gz || {
+    echo 'curl -O http://addresses.loyce.club/blockchair_bitcoin_addresses_and_balance_LATEST.tsv.gz failed';
+    exit 1;
+}
+echo 'downloaded'
+
+# gunzip
+echo 'gunzip is started...'
+gunzip blockchair_bitcoin_addresses_and_balance_LATEST.tsv.gz || {
+    echo 'gunzip Bitcoin_addresses_LATEST.txt.gz failed';
+    exit 1;
+}
+echo 'gunziped'
 
 # terminal current memcached
 sh terminate.sh
 
 # replace data.tsv
-sh replace_data.sh
+# remove previous data.txt
+echo 'removing previous data.tsv...'
+rm data.tsv
+echo 'deleted'
 
-# clean up
-sh clean_useless_files.sh
+# put new data.tsv
+echo 'renaming new addresses to data.txt...'
+mv blockchair_bitcoin_addresses_and_balance_LATEST.tsv data.tsv || {
+    echo 'mv blockchair_bitcoin_addresses_and_balance_LATEST.tsv data.tsv';
+    exit 1;
+}
+echo 'renamed'
+
+clean_useless_files
 
 # starting magic
 sh start.sh
