a6f7594e76ba9f932bdae788cf85381b63b79c61
Dmitriy Molokov <dmitriymolokov@users.noreply.github.com>
2022-09-09 11:41:50 +0300

    added restart.sh

---
+++ restart.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ID=fhmukgk37ig5dehv3a50
+
+echo 'the instance is being stop'
+yc compute instance stop $ID
+
+echo 'starting the instance...'
+yc compute instance start $ID
+
+echo 'connecting to console...'
+yc compute connect-to-serial-port --instance-id $ID --user admin --ssh-key ~/.ssh/id_ed25519
