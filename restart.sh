eb41bda17c5a17042868863ec1b5e1f4ffc41320
Dmitriy Molokov <dmitriymolokov@users.noreply.github.com>
2022-09-13 21:52:18 +0300

    wip

--- restart.sh
+++ restart.sh
@@ -5,10 +5,14 @@ ID=fhmukgk37ig5dehv3a50
 echo 'the instance is being stop'
 yc compute instance stop $ID
 
-sleep 30
+echo 'sleep 61'
+sleep 61
 
 echo 'starting the instance...'
 yc compute instance start $ID
 
+echo 'sleep 10'
+sleep 10
+
 echo 'connecting to console...'
-yc compute connect-to-serial-port --instance-id $ID --user admin --ssh-key ~/.ssh/id_ed25519
+yc compute connect-to-serial-port --instance-id $ID --user admin --ssh-key /Users/christopher/.ssh/id_ed25519
